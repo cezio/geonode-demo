c66ee36e9c79c5fa0112ba39455b49d7f5e0ee2f
Cezary Statkiewicz <cezio@thelirium.net>
2018-04-27 14:01:33 +0200

    split backup script

--- master.demo.geonode.org/scripts/restore.sh
+++ master.demo.geonode.org/scripts/restore.sh
@@ -5,11 +5,11 @@ source $(dirname $0)/vars.sh
 # restart compose
 docker-compose up -d
 
-until docker-compose exec ${DB_SERVICE} psql -U postgres -d geonode -c 'select * from people_profile;' ; do
-  >&2 echo "Postgres is unavailable - sleeping"
+until docker-compose exec ${DB_SERVICE} psql -P pager=off -U postgres -d geonode -c 'select * from people_profile;' > /dev/null ; do
   sleep 1
 done
 
+echo "restoring data from ${AUTH_BACKUP_PATH}"
 
 # push auth data and restore them
 docker cp ${AUTH_BACKUP_PATH} ${GEONODE_CONTAINER}:/root/
