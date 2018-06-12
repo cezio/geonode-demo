dd53c090b728ffff37ba58516d2e0dac53d56ebc
Cezary Statkiewicz <cezio@thelirium.net>
2018-06-12 15:59:27 +0200

    do not run migrations on celery #25

--- master.demo.geonode.org/entrypoint.sh
+++ master.demo.geonode.org/entrypoint.sh
@@ -16,17 +16,18 @@ echo GEOSERVER_PUBLIC_LOCATION=$GEOSERVER_PUBLIC_LOCATION
 echo "waitfordbs task done"
 
 echo "running migrations"
-/usr/local/bin/invoke migrations
-echo "migrations task done"
-
-if [ ! -e "/mnt/volumes/statics/geonode_init.lock" ]; then
-    /usr/local/bin/invoke statics
-    /usr/local/bin/invoke prepare
-    echo "prepare task done"
-    /usr/local/bin/invoke fixtures
-    echo "fixture task done"
-fi
-/usr/local/bin/invoke initialized
+# do not run in celery (it reuses container, but doesn't need to run db setup)
+if [ 'true' != ${IS_CELERY} ]; then
+    /usr/local/bin/invoke migrations
+    if [ ! -e "/mnt/volumes/statics/geonode_init.lock" ]; then
+        /usr/local/bin/invoke statics
+        /usr/local/bin/invoke prepare
+        echo "prepare task done"
+        /usr/local/bin/invoke fixtures
+        echo "fixture task done"
+    fi
+    /usr/local/bin/invoke initialized
+fi;
 
 set -e
 echo DOCKER_ENV=$DOCKER_ENV
