d4a6dc33f258f8016730139b0a7a4616d9d4b7fc
Cezary Statkiewicz <cezio@thelirium.net>
2018-06-18 10:01:07 +0200

    monitoring enabled by default, dump after auth

--- master.demo.geonode.org/scripts/backup.sh
+++ master.demo.geonode.org/scripts/backup.sh
@@ -5,12 +5,9 @@ source $(dirname $0)/vars.sh
 
 # dump auth and monitoring in geonode
 docker-compose exec -T ${GEONODE_SERVICE} python manage.py dumpdata people.Profile -o /root/auth.json
-docker-compose exec -T ${GEONODE_SERVICE} python manage.py dumpdata monitoring -o /root/monitoring.json
-
-
-# get data from container into storage
 docker cp ${GEONODE_CONTAINER}:/root/auth.json $AUTH_BACKUP_PATH
-docker cp ${GEONODE_CONTAINER}:/root/monitoring.json $MONITORING_BACKUP_PATH
 docker exec ${GEONODE_CONTAINER} rm /root/auth.json
-docker exec ${GEONODE_CONTAINER} rm /root/monitoring.json
 
+docker-compose exec -T ${GEONODE_SERVICE} python manage.py dumpdata monitoring -o /root/monitoring.json
+docker cp ${GEONODE_CONTAINER}:/root/monitoring.json $MONITORING_BACKUP_PATH
+docker exec ${GEONODE_CONTAINER} rm /root/monitoring.json
